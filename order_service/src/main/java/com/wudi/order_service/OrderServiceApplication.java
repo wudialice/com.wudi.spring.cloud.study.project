10a1751148be919524796e24005abc97b2b95e12
wudi <913737343@qq.com>
2019-06-03 17:49:26 +0800

    use hystrix

--- order_service/src/main/java/com/wudi/order_service/OrderServiceApplication.java
+++ order_service/src/main/java/com/wudi/order_service/OrderServiceApplication.java
@@ -2,11 +2,14 @@ package com.wudi.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+//Hystrix熔断降级
+@EnableCircuitBreaker
 //使用Feign负载策略
 @EnableFeignClients
 @SpringBootApplication
--- order_service/src/main/java/com/wudi/order_service/controller/OrderController.java
+++ order_service/src/main/java/com/wudi/order_service/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.wudi.order_service.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.wudi.order_service.domain.ProductOrder;
 import com.wudi.order_service.service.ProductOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,9 @@ import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/order")
@@ -32,11 +36,20 @@ public class OrderController {
         return productOrderService.save(userId,productId);
     }
 
-
     @RequestMapping("/saveF")
-    public Object saveF(@RequestParam("user_id") int userId,
-                       @RequestParam("product_id") int productId
+    @HystrixCommand(fallbackMethod = "saveOrFail")    public Object saveF(@RequestParam("user_id") int userId,
+                                                                          @RequestParam("product_id") int productId
     ){
-        return useFeignProductService.save(userId,productId);
+        Map<String,Object> data = new HashMap<>();
+        data.put("code",0);
+        data.put("data",useFeignProductService.save(userId,productId));
+        return data;
+    }
+
+    private Object saveOrFail(int userId,int productId){
+        Map<String,Object> msg = new HashMap<>();
+        msg.put("code",-1);
+        msg.put("msg","抢购的人数太多了，您被挤出来了，稍等重试");
+        return  msg;
     }
 }
